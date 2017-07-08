fdf03c172d42e11eb44d10a7b72c4a82ca03d547
Tiffany Jann <tiffanyjann@berkeley.edu>
2017-07-08 08:57:54 +0800

    added /index path, and comments

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
 from app import app
 from flask import render_template
 
+# INDEX returns a string, to be displayed on the client's web browser. 
+# The two route decorators above the function create the mappings from URLs / and /index to this function
 
 @app.route('/', methods=['GET', 'POST'])
+@app.route('/index')
 def index():
     return render_template("index.html", title='Home')
--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 #!env/bin/python
 
+# The script simply imports the app variable
+# from our app package and invokes its run method to start the server.
+# Remember that the app variable holds the env instance.
+
 from app import app
 
 app.run(debug=True)

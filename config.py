2f989d95ae953f3b94577293a41d122a50df4140
Tiffany Jann <tiffanyjann@berkeley.edu>
2017-07-08 08:03:57 +0800

    fixed db path to point at db

--- config.py
+++ config.py
@@ -5,4 +5,4 @@ import os
 WTF_CSRF_ENABLED = False
 SECRET_KEY = 'you-will-never-guess'
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
-DATABASE = os.path.join(PROJECT_ROOT, 'sqlite', 'paperDB.db')
+DATABASE = os.path.join(PROJECT_ROOT, 'db', 'XchangeFinder.db')

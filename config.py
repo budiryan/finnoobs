9ebe93d0d974b16fcbfa04c3b471089d11757d55
KP-Kepra <kevinpr153@gmail.com>
2017-07-08 12:16:08 +0800

    secret key fix

--- config.py
+++ config.py
@@ -7,6 +7,7 @@ import os
 WTF_CSRF_ENABLED = False
 PROJECT_ROOT = os.path.dirname(os.path.realpath(__file__))
 DATABASE = os.path.join(PROJECT_ROOT, 'db', 'XchangeFinder.db')
+SESSION_TYPE = 'filesystem'
 
 
 OPENID_PROVIDERS = [

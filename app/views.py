8e571c07c5b96417cf37dd9ecdaaf4caa0d4c2d5
budiryan <budiryan@github.com>
2017-07-08 12:18:05 +0800

    remove openid from config

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from app import app
-from flask import render_template, g, request, flash, redirect
+from flask import Flask, render_template, g, request, flash, redirect, url_for
 from .forms import LoginForm
 import sqlite3
 
@@ -86,18 +86,10 @@ def user(username):
 # LOGIN PAGE
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-	# instantiate LoginForm object
-    form = LoginForm()
-    # validate_on_submit returns true
-    # - a form submission request
-    # - if all the validators attached to fields are all right
-    # indicating that the data is valid and can be processed
-    if form.validate_on_submit():
-        flash('Login requested for OpenID="%s", remember_me=%s' %
-              (form.openid.data, str(form.remember_me.data)))
-        return redirect('/index')
-    # send LoginForm object down to the template login.html
-    return render_template('login.html', 
-                           title='Sign In',
-                           form=form,
-                           providers=app.config['OPENID_PROVIDERS'])
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
